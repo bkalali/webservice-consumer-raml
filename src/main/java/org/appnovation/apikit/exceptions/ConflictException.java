7509ccb7e2c7065b80afbf383df8951aa7d66bf3
Bahman Kalali <bahman@appnovation.com>
2015-08-04 11:30:55 -0500

    Added ConflictException for translating from SOAP to ApiKit Exception

---
+++ src/main/java/org/appnovation/apikit/exceptions/ConflictException.java
@@ -0,0 +1,15 @@
+
+package org.appnovation.apikit.exceptions;
+
+import org.mule.module.apikit.exception.MuleRestException;
+
+
+
+public class ConflictException extends MuleRestException {
+
+    private static final long serialVersionUID = 3387516993124229969L;
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
